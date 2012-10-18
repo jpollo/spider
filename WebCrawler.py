9be251bd100bfaba665f5aa0c1e841885eb6f05b
jasonllinux <jasonllinux@gmail.com>
2012-10-18 22:02:44 +0800

    add

--- WebCrawler.py
+++ WebCrawler.py
@@ -3,50 +3,51 @@
 import threading
 import GetUrl
 import urllib
+import os
+from bs4 import BeautifulSoup
+import ParsUrl
 
-g_mutex = threading.Lock()
+g_mutex = threading.Lock()   #互斥锁
 g_pages = []        #下载的页面
 g_dledUrl = []      #下载过的页面
 g_toDlUrl = []        #当前要下载的页面
 g_failedUrl = []       #下载失败的页面
-g_totoalCount = 0    #下载过的页面数量
+g_totalcount = 0    #下载过的页面数量
 
 
 class WebCrawler:
 
     def __init__(self, thNumber):
-        self.thNum = thNumber
+        self.thNumber = thNumber
+        self.threadPool = []
+        self.logfile = file(os.getcwd()+'/'+'log'+'/'+'#log.txt', 'w')
+        print self.logfile
 
-    def craw():
-        while len(urlList) != 0:
-            init_url_list()
-            DownLoad_list()
-            Update_list()
- 
     def download(self, url, fileName):
+        print 'download==============='
+        print 'url',url
+        print 'fileName',fileName
         Cth = CrawlerThread(url, fileName)
         self.threadPool.append(Cth)
         Cth.start()
-    
+
     def downloadAll(self):
+        print 'download All'
         global g_toDlUrl
         global g_totalcount
         i = 0
         while i < len(g_toDlUrl):
             j = 0
-            while j < self.threadNumber and i + j < len(g_toDlUrl):
+            while j < self.thNumber and i + j < len(g_toDlUrl):
                 g_totalcount += 1    #进入循环则下载页面数加1
-                self.download(g_toDlUrl[i+j],str(g_totalcount)+'.htm')
-                print 'Thread started:',i+j,'--File number = ',g_totalcount
+                self.download(g_toDlUrl[i+j], os.getcwd()+'/' + 'down_html'+ '/' + str(g_totalcount)+'.htm')
+                print 'Thread started:', i+j, '--File number = ', g_totalcount, '\n'
                 j += 1
             i += j
             for th in self.threadPool:
                 th.join(30)     #等待线程结束，30秒超时
             self.threadPool = []    #清空线程池
         g_toDlUrl = []    #清空列表
-
-
-
 
     def updateToDl(self):
         global g_toDlUrl
@@ -55,16 +56,19 @@ class WebCrawler:
         for s in g_pages:
             newUrlList += GetUrl.GetUrl(s)   #######GetUrl要具体实现
         g_toDlUrl = list(set(newUrlList) - set(g_dledUrl))    #提示unhashable
-                
-    def Craw(self,entryUrl):    #这是一个深度搜索，到g_toDlUrl为空时结束
+
+    def Craw(self, entryUrl):    #这是一个深度搜索，到g_toDlUrl为空时结束
         g_toDlUrl.append(entryUrl)
         depth = 0
         while len(g_toDlUrl) != 0:
             depth += 1
-            print 'Searching depth ',depth,'...\n\n'
+            print 'Searching depth ', depth, '...\n\n'
+            #download all
             self.downloadAll()
+            #update to download
             self.updateToDl()
-            content = '\n>>>Depth ' + str(depth)+':\n'                         ##（该标记表示此语句用于写文件记录）
+            content = '\n>>>Depth ' + str(depth)+':\n'
+            #write                        ##（该标记表示此语句用于写文件记录）
             self.logfile.write(content)                                        ##
             i = 0                                                              ##
             while i < len(g_toDlUrl):                                          ##
@@ -81,16 +85,24 @@ class CrawlerThread(threading.Thread):
         threading.Thread.__init__(self)
         self.url = url
         self.fileName = fileName
-        
+
     def run(self):
+        print 'Thread run============ \n'
         global g_mutex
         global g_failedUrl
         global g_dledUrl
+
         try:
+            print 'Thread run== \n'
+            #print 'url',self.url
             f = urllib.urlopen(self.url)
+            print 'url open sucess'
             s = f.read()
+            ParsUrl.getUrl(s)
+            print 'read sucess'
             fout = file(self.fileName, 'w')
             fout.write(s)
+            print 'write sucess'
             fout.close()
         except:
             g_mutex.acquire()
@@ -99,10 +111,8 @@ class CrawlerThread(threading.Thread):
             g_mutex.release()
             print 'Failed downloading and saving', self.url
             return None
-        
+
         g_mutex.acquire()
         g_pages.append(s)
         g_dledUrl.append(self.url)
         g_mutex.release()
-       
-        
--- test.py
+++ test.py
@@ -5,9 +5,14 @@
 import WebCrawler
 
 if __name__ == '__main__':
+
     print '==========Start============'
-    url = raw_input('set url(Example:www.jasonllinux.com): \n')
-    thNum = int(raw_input('set num of threads： \n'))
-    
+#    url = raw_input('set url(Example:www.jasonllinux.com): \n')
+#    thNum = int(raw_input('set num of threads： \n'))
+    url = 'http://www.2tu.cc/'
+    thNum = 1
     #print 'url',url
     #print 'thNum',thNum
+
+    wc = WebCrawler.WebCrawler(thNum)
+    wc.Craw(url)
